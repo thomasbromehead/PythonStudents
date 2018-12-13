8d76874520a9558522e6f98ce105a2dcb05fc1df
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-13 11:32:27 +0100

    Tiny method for adding a grade to a student :sparkles:

--- dict.py
+++ dict.py
@@ -1,4 +1,4 @@
-def add_marks():
+def create_student():
     name = input('Enter your student\'s name: ')
     marks = input("Enter student's grades as a comma separated list of values: ")
     marks_array = marks.split(',')
@@ -6,7 +6,17 @@ def add_marks():
         "name":name,
         "grades": [int(grade) for grade in marks_array]
     }
-
     return student_details
 
-print(add_marks())
+def add_mark(student, mark):
+    student["grades"].append(mark)
+
+
+s = create_student()
+print(s)
+
+
+add_mark(s, 28)
+
+
+print(s)
