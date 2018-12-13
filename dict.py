4c7abd63cc74bbb1a5cd5ebaa1b1795b3c65e585
Thomas Bromehead <t_bromehead@yahoo.fr>
2018-12-13 11:12:05 +0100

    Use of list comprehension :sparkles:

--- dict.py
+++ dict.py
@@ -1,2 +1,12 @@
-sample = {"name":7, 5:2, "marks":[2,5,4], 1:2}
-print(sample["marks"][len(sample["marks"])-1])
+def add_marks():
+    name = input('Enter your student\'s name: ')
+    marks = input("Enter student's grades as a comma separated list of values: ")
+    marks_array = marks.split(',')
+    student_details = {
+        "name":name,
+        "grades": [int(grade) for grade in marks_array]
+    }
+
+    return student_details
+
+print(add_marks())
